5256a5fcab875b57040e08c44f2bded28f28f7db
CyberVerve07 <adityamahajan7287@gmail.com>
2026-05-20 19:06:44 +0530

    chore: prune obsolete compiled javascript files and redundant lockfiles

--- server/routes/dietRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dietController = require('../controllers/dietController');
-const { auth } = require('../middleware/auth');
-
-router.get('/', auth, dietController.getDietPlans);
-router.post('/', auth, dietController.createDietPlan);
-
-module.exports = router;
--- server/routes/icuRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const icuController = require('../controllers/icuController');
-const { auth, authorize } = require('../middleware/auth');
-
-router.get('/', auth, authorize('Admin', 'Doctor'), icuController.getICUPatients);
-router.patch('/:id', auth, authorize('Admin', 'Doctor'), icuController.updateICUMetrics);
-
-module.exports = router;
